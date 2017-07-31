0e1f8767e7362022f82831194bb3fe718f5d69d3
macubis <macubis@gmail.com>
2017-07-31 02:09:07 -0400

    subiendo fotos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-main.js
+function titulo(){
+var title = document.getElementByClassName("items");
+	var mostrar = datos.forEach(function(elemento){
+    [elemento].innerHTML += "<h4>"+ elemento.title + "</h4>";
+});
+mostrar;
+}
+titulo();
